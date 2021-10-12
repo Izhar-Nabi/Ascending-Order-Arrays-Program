a156249301d3f9af09227edced2215637b00f60d
Izhar-Zour <90437474+Izhar-Nabi@users.noreply.github.com>
2021-10-12 19:27:17 +0500

    DisAcending Number Order Program

---
+++ ArrayDisAscending.java
@@ -0,0 +1,18 @@
+class ArrayDisAscending{
+	public static void main(String args[]){
+	int A[]={0, 3, 5, 9, 7, 4, 8, 2, 1, 6};
+	int i,temp;
+	for ( i=0; i<A.length; i++){
+	for(int j=i+1; j<A.length; j++){
+	if(A[i]<A[j]){
+	temp=A[i];
+	A[i]=A[j];
+	A[j]=temp;
+	}
+			}
+		}
+	for(int n : A){
+	System.out.print(n+" ");}
+
+	}
+}
